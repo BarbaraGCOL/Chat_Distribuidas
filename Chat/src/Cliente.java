d260463ac9e582c0b049c21ee2cacf0e411b0836
BarbaraGCOL <barbara.gcol@gmail.com>
2015-04-15 19:29:46 -0300

    JSON
    Inserção do tráfego de mensagens via JSON

---
+++ Chat/src/Cliente.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import org.json.JSONObject;
+
+public class Cliente extends Thread {
+    
+    private final Socket conexao;
+    
+    public Cliente(Socket socket) {
+        this.conexao = socket;
+    }
+    public static void main(String args[])
+    {
+        try {
+            // conecta a IP do Servidor, Porta
+            Socket socket = new Socket("127.0.0.1", 7000);
+            
+            PrintStream saida = new PrintStream(socket.getOutputStream());
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+           
+            System.out.print("Digite seu nome: ");
+            String meuNome = teclado.readLine();
+            
+            conectar(saida, meuNome.toUpperCase());
+            
+            Thread thread = new Cliente(socket);
+            thread.start();
+            
+            String texto;
+            
+            while (true)
+            {
+                System.out.print(" > ");
+                try
+                {
+                	texto = teclado.readLine();
+                    JSONObject mensagem = new JSONObject();
+                    
+                    if(texto.equals("~LISTAR")) {
+                    	mensagem.put("OPERACAO", "LISTAR");
+                    }
+                    else
+                    	if(texto.equals("~SAIR")) {
+                    		mensagem.put("OPERACAO", "SAIR");
+                    	}
+                    	else {
+                    		mensagem.put("OPERACAO", "ENVIAR");
+                    	}
+                    	
+                    mensagem.put("MENSAGEM", texto);
+                    saida.println(mensagem.toString());
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        }
+    }
+    // execução da thread
+    @Override
+    public void run()
+    {
+        try {
+            //recebe mensagens de outro cliente através do servidor
+            BufferedReader entrada = 
+                new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            
+            String msg;
+            while (true)
+            {
+                // pega o que o servidor enviou
+                msg = entrada.readLine();
+            
+                if (msg == null) {
+                    System.out.println("CONEXÃO ENCERRADA!");
+                    System.exit(0);
+                }
+//                System.out.println();
+                System.out.println(msg);
+                System.out.print(" > ");
+            }
+        } catch (IOException e) {
+            System.out.println("Ocorreu uma Falha... .. ." + 
+                " IOException: " + e);
+        }
+    }
+    
+    public static boolean conectar(PrintStream saida, String nomeUsuario)
+    {
+        try
+        {
+            JSONObject mensagem = new JSONObject();
+            mensagem.put("OPERACAO", "CONECTAR");
+            mensagem.put("MENSAGEM", nomeUsuario);
+            saida.println(mensagem.toString());
+            return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+    }
+}
---
+++ Chat/src/Servidor.java
@@ -0,0 +1,199 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Servidor extends Thread {
+
+    private static Map<String, PrintStream> CLIENTES;
+    private Socket conexao;
+    private String nomeCliente;
+    private static final List<String> CONECTADOS = new ArrayList<>();
+    
+    public Servidor(Socket socket) {
+        this.conexao = socket;
+    }
+    
+    /**
+     * Faz login do cliente
+     * @param newName 
+     * @return true se já existe usuário logado com o respectivo nome
+     */
+    public boolean verificaLogin(String newName) {
+        // Verifica se já existe usuário logado com esse nome
+        if (CONECTADOS.stream().anyMatch((nome) -> (nome.equals(newName)))) {
+            return true;
+        }
+        CONECTADOS.add(newName);
+        return false;
+    }
+    
+    /**
+     * Faz loggof do cliente
+     * @param oldName 
+     */
+    public void logoff(String oldName, PrintStream saida) {
+        for (int i = 0; i < CONECTADOS.size(); i++) {
+            if (CONECTADOS.get(i).equals(oldName))
+                CONECTADOS.remove(oldName);
+        }
+        CLIENTES.remove(this.nomeCliente);
+        System.out.println(this.nomeCliente + " saiu do bate-papo!");
+        String[] out = {" do bate-papo!"};
+        send(saida, out[0]);
+        try {
+            getConexao().close();
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Lista usuários logados no sistema
+     * @param saida 
+     */
+    public void listaUsuarios(PrintStream saida){
+        try
+        {
+            JSONObject mensagem = new JSONObject();
+            mensagem.put("OPERACAO", "LISTAR");
+            mensagem.put("MENSAGEM", CONECTADOS.size() + " usu�rios conectados: " + CONECTADOS.toString());
+            saida.println(mensagem.toString());
+        }
+		catch (Exception e)
+		{
+		}
+    }
+    
+    public static void rodarServer(int porta){
+        CLIENTES = new HashMap<String, PrintStream>();
+        try {
+            ServerSocket server = new ServerSocket(porta);
+            System.out.println("ServidorSocket rodando na porta " + porta);
+            while (true) {
+                Socket conexao = server.accept();
+                Thread t = new Servidor(conexao);
+                t.start();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+    
+    public static void main(String args[]) {
+       int porta = 7000;
+       rodarServer(porta);
+    }
+    
+    @Override
+    public void run() {
+        try {
+            BufferedReader entrada = 
+                new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+            PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+            
+            JSONObject json = null;
+            
+            String operacao = "";
+          
+            do{
+            	
+            	try {
+                    json = new JSONObject(entrada.readLine());
+                } catch (JSONException ex) {
+                    Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            	
+            	operacao = (json.get("OPERACAO")).toString();
+            	
+                String msg;
+
+                switch(operacao){
+                    case "CONECTAR":
+                        setNomeCliente((json.get("MENSAGEM")).toString());
+                        conectar(saida, getNomeCliente());
+                        break;
+                    case "ENVIAR":
+                        msg = (json.get("MENSAGEM")).toString();
+                        send(saida, msg);
+                        break;    
+                    case "LISTAR":
+                    	listaUsuarios(saida);
+                        break;
+                    case "SAIR":
+                        logoff(getNomeCliente(), saida);
+                        break;
+                    default:
+                        break;
+                }
+            }while(!operacao.equals("SAIR"));
+        
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        } catch (JSONException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * 
+     * @param saida
+     * @param acao
+     * @param msg 
+     */
+    public void send(PrintStream saida, String msg) {
+        for (Map.Entry<String, PrintStream> cliente : CLIENTES.entrySet()) {
+            
+            PrintStream chat = cliente.getValue();
+            
+            if (chat != saida) {
+                chat.println(this.nomeCliente +" disse: "+ msg);
+            }
+        }
+    }
+    
+    public boolean conectar(PrintStream saida, String nomeUsuario)
+    {
+        if (verificaLogin(nomeUsuario)) {
+            saida.println("Este nome ja existe! Conecte novamente com outro Nome.");
+            try {
+                getConexao().close();
+            } catch (IOException ex) {
+                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return false;
+        } else {
+            //mostra o nome do cliente conectado ao servidor
+            System.out.println(this.nomeCliente + " : Conectado ao Servidor!");
+        }
+        if (nomeUsuario == null || nomeUsuario.equals("")) {
+            return false;
+        }
+        
+        CLIENTES.put(this.nomeCliente, saida);
+            
+        return true;
+    }
+    
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    
+    public Socket getConexao() {
+        return conexao;
+    }
+
+    public void setConexao(Socket conexao) {
+        this.conexao = conexao;
+    }
+}
